32d63ccd00d9c755e15de78cde9d0dfd12f9286d
Lockywolf <lockywolf@gmail.com>
2020-03-24 23:52:46 +0800

    type dispatch 5.51

--- second.f90
+++ second.f90
@@ -1,4 +1,4 @@
-  ! Time-stamp: <2020-03-23 13:13:37 lockywolf>
+  ! Time-stamp: <2020-03-24 23:52:00 lockywolf>
   ! Author: lockywolf gmail.com
   ! A rudimentary scheme interpreter
 
@@ -24,17 +24,18 @@ contains
           !print *, "end of file"
           exit
        else if (reason > 0) then
-          print*, "read error"
+          print*, "Fortran:read error"
           dummy = c_exit(1) 
        else
           retval = retval//buffer;
        end if
     end do
-    print *, "debug:", retval, new_line('a')
+    !print *, "debug:", retval, new_line('a')
   end function read_until_eof
 end module system_interface
 
 module scheme
+  use, non_intrinsic :: system_interface, only: c_exit
   integer, parameter :: memory_size = 1024
   integer, parameter :: strings_pool_size = 1024
   integer, dimension(memory_size) :: the_cars
@@ -43,11 +44,29 @@ module scheme
 
 
   type, abstract :: scheme_object
-     integer :: value
+     class(*), allocatable :: value
+   contains
+     procedure, pass :: generic_scheme_print => print_scheme_object
+     generic, public :: write (formatted) => generic_scheme_print
   end type scheme_object
+
+  !  abstract interface
+  !     subroutine scheme_object_printer(this, unit, iotype, v_list, iostat, iomsg)
+  !       import :: scheme_object
+  !          class(scheme_object), intent(in) :: this
+  !          integer, intent(in)         :: unit
+  !          character(*), intent(in)    :: iotype
+  !          integer, intent(in)         :: v_list (:)
+  !          integer, intent(out)        :: iostat
+  !          character(*), intent(inout) :: iomsg
+  !    end subroutine scheme_object_printer
+  ! end interface
 
   
   type, extends( scheme_object ) :: scheme_string
+   contains
+     procedure :: generic_scheme_print => print_scheme_string
+     !      generic :: write (formatted) => print_scheme_string
   end type scheme_string
   type, extends( scheme_object ) :: scheme_pair
   end type scheme_pair
@@ -60,10 +79,11 @@ module scheme
   type, extends( scheme_object ) :: scheme_true
   end type scheme_true
   type, extends( scheme_object ) :: scheme_primitive_procedure
-     procedure (func), pointer, nopass :: operation => null()
   end type scheme_primitive_procedure
-   
-  interface
+
+
+  
+  interface ! TODO:I want to use something like this for primitive procedures, but not sure
      function func( arguments ) result( retval )
        import scheme_object
        class(scheme_object), pointer :: retval
@@ -73,20 +93,123 @@ module scheme
 
  
 contains
+  subroutine print_scheme_object(this, unit, iotype, v_list, iostat, iomsg)
+    class(scheme_object), intent(in) :: this
+    integer, intent(in)         :: unit
+    character(*), intent(in)    :: iotype
+    integer, intent(in)         :: v_list (:)
+    integer, intent(out)        :: iostat
+    character(*), intent(inout) :: iomsg
+    write (unit, fmt=*, iostat=iostat, iomsg=iomsg) "#<unknown scheme object>"
+    !print *, new_line('a'), "trace:print_scheme_object"
+    !print *, new_line('a'), "#<unknown scheme object>"
+    ! select type ( temp => this%value )
+    ! type is (character(len=*))
+    !    write (unit, fmt=*, iostat=iostat, iomsg=iomsg) temp
+    ! class default
+    !    print *, 'error'
+    !    iostat = c_exit(1)
+    ! end select
+  end subroutine print_scheme_object
+
+  subroutine print_scheme_string(this, unit, iotype, v_list, iostat, iomsg)
+    class(scheme_string), intent(in) :: this
+    integer, intent(in)         :: unit
+    character(*), intent(in)    :: iotype
+    integer, intent(in)         :: v_list (:)
+    integer, intent(out)        :: iostat
+    character(*), intent(inout) :: iomsg
+    select type ( temp => this%value )
+    type is (character(len=*))
+       write (unit, fmt=*, iostat=iostat, iomsg=iomsg) '#<scheme_string "' // temp // '">'
+    class default
+       print *, 'error'
+       iostat = c_exit(1)
+    end select
+  end subroutine print_scheme_string
+  
+  function remove_junk( arg ) result( intermediate )
+    character(:), intent(in), allocatable :: arg
+    character(:), allocatable :: intermediate
+    integer :: i
+    logical :: comment_flag = .false.
+    character, parameter :: newline = new_line('a')
+    print *, new_line('a'), "trace:remove_junk"
+    print *, new_line('a'), "remove_junk:arg=", arg
+    intermediate = ""
+    do i=1,len(arg)
+       if (arg(i:i) == ';') then
+          comment_flag = .true.
+       else if (arg(i:i) == newline) then
+          comment_flag = .false.
+       end if
+       if ((arg(i:i) == newline) .or. comment_flag) then
+          cycle
+       end if
+       intermediate = intermediate//arg(i:i)
+    end do
+    !print *, new_line('a'), "remove_junk:result=", intermediate
+  end function remove_junk
+
+  subroutine parse_string( arg, token, rest )
+    implicit none
+    character(:), pointer, intent(in) :: arg
+    character(:), pointer, intent(out) :: rest
+    class(scheme_object), pointer, intent(out) :: token
+    integer :: caret = 1
+    character(:), allocatable :: interim_string
+    interim_string = ""
+    allocate( scheme_string :: token )
+    allocate( character :: token%value )
+    token%value = "BUG1"
+    caret = 2 ! skipping the first quotation mark '"'
+    do
+       if (arg(caret:caret) == '"') then
+          exit
+       end if
+       interim_string = interim_string // arg(caret:caret)
+       caret = caret + 1
+    end do
+    token%value = interim_string
+    rest => arg(caret+1:)
+  end subroutine parse_string
+  
+  function parse_sexp( arg ) result( retval )
+    character(:), allocatable, target :: arg
+    character(:), pointer :: arg_pointer 
+    class(scheme_object), pointer :: retval
+    class(scheme_object), pointer :: token
+
+    select case (arg(1:1))
+    case ('(')
+       print *, "debug: parsing list"
+    case ('"')
+       print *, "debug: parse string", new_line('a')
+       arg_pointer => arg
+       call parse_string( arg_pointer, token, arg_pointer )
+       retval => token
+    case ("'")
+       print *, "debug: parse quote"
+    case ( '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+       print *, "debug: parse number"   
+    case default
+       print *, "debug: parse symbol"
+    end select
+    
+  end function parse_sexp
 
 
   
-
 end module scheme
 
 program main
   use :: scheme, only : scheme_object, scheme_string, scheme_pair, &
-       scheme_symbol
+       scheme_symbol, remove_junk, parse_sexp
   use, intrinsic :: iso_fortran_env
   use, non_intrinsic :: system_interface, only: c_exit, read_until_eof
   implicit none
-  integer :: counter = 0
   integer :: fake = 0
+  class(scheme_object), pointer :: parsed_expression  
   character(len=:), allocatable :: test_string
   !class(scheme_object), pointer :: test_object
   !test_object => read_sexp()
@@ -97,7 +220,10 @@ program main
   !   nullify( test_object )
   !   if (counter < 3) goto 001
   test_string =  read_until_eof()
-  print *, "debug", test_string
+  !print *, "debug", test_string
+  test_string = remove_junk( test_string )
+  parsed_expression => parse_sexp( test_string ) ! test 1, should return a scheme_string
+  print *, parsed_expression
   fake = c_exit(0)
   stop 0
 contains
