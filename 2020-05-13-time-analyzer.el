a4a2752c922cba33088bbedf4ee9bdd4ec9023f0
Lockywolf <lockywolf@gmail.com>
2020-06-28 16:29:59 +0800

    Clean experience report

--- 2020-05-13-time-analyzer.el
+++ 2020-05-13-time-analyzer.el
@@ -94,6 +94,7 @@
 
 ;; lexical-defun
 (get-task-sequence-data ()
+(let ((problem-index 0))
  (apply
   'seq-concatenate
   (cons
@@ -116,12 +117,14 @@
                ;; (princ (char-to-string ?\n) (get-buffer "*scratch*"))
                (assert (org-element-property :raw-value x) t "error: x=%s" x)
                (assert (org-element-property :closed x) t "error: x=%s" x)
+               (setq problem-index (+ 1 problem-index))
                (list (list (org-element-property :raw-value x)
-                           (org-element-property :closed x))))
-                      (list)))))))
+                           (org-element-property :closed x)
+                           problem-index)))
+                      (list))))))))
  )
 
-;; lexical-define
+;; lexical-defun
 (sort-task-seq (task-seq)
  (seq-sort
   (lambda (x y)
@@ -132,7 +135,7 @@
   task-seq)
  )
 
-;; lexical-define
+;; lexical-defun
 (find-out-of-order-tasks (task-seq)
 (seq-reduce (lambda (acc next-elem)
               (assert next-elem t "next-elem=%s" next-elem)
@@ -151,6 +154,162 @@
             (list 0 "2019-08-19 Mon 09:19" (list) (list)))
 )
 
+;; lexical-defun
+(find-spanning-sessions-and-duration
+ (prev-time-stamp next-time-stamp study-sessions)
+ (seq-reduce
+  (lambda (acc next-session)
+    (let ((session-start (car next-session))
+          (session-end (cadr next-session)))
+      (cond ((<= session-end prev-time-stamp) acc)
+            ((<= next-time-stamp session-start ) acc)
+            (t (list (+ (car acc) 1)
+                     (+ (cadr acc)
+                        (cond ((and (<= prev-time-stamp session-start)
+                                    (<= session-end next-time-stamp))
+                               (- session-end session-start))
+                              ((and (<= session-start prev-time-stamp)
+                                    (<= prev-time-stamp session-end)
+                                    (<= session-end next-time-stamp))
+                               (- session-end prev-time-stamp))
+                              ((and (<= prev-time-stamp session-start)
+                                    (<= session-start next-time-stamp)
+                                    (<= next-time-stamp session-end))
+                               (- next-time-stamp session-start))
+                              ((and (<= session-start prev-time-stamp)
+                                    (<= next-time-stamp session-end))
+                               (- next-time-stamp prev-time-stamp))
+                              (t 0))))))
+      
+      ;; (if (and
+      ;;      (<= session-start next-time-stamp)
+      ;;      (<= next-time-stamp session-end))
+      ;;     t
+      ;;   acc)
+      ))
+  study-sessions
+  (list 0 0)))
+
+;; lexical-defun
+(summarize-list (sorted-task-seq study-sessions)
+(cadr (seq-reduce
+       (lambda (acc next-elem)
+         (let ((prev-time-stamp (car acc))
+               (retval (cadr acc))
+               (next-time-stamp
+                (org-time-string-to-seconds
+                 (org-element-property :raw-value (cadr next-elem))))
+               (exercise-name (car next-elem))
+               (exercise-index (caddr next-elem)))
+           ;; loop body
+           (let ((spans-sessions
+                  (find-spanning-sessions-and-duration
+                   prev-time-stamp next-time-stamp study-sessions)))
+             (list next-time-stamp
+                   (cons (list exercise-name  ;; next-elem
+                               :spent-time-calendar-days (/ (-
+                                                             next-time-stamp
+                                                             prev-time-stamp)
+                                                            (* 60 60 24))
+                               :spans-sessions (if (not (eq 0 (car spans-sessions)))
+                                                   (car spans-sessions)
+                                                 (error "Fix time: %s, spans-sessions=%s" next-elem spans-sessions))
+                               :spent-time-net-minutes (/ (cadr spans-sessions) 60)
+                               :original-index exercise-index)
+                                 retval)))))
+       sorted-task-seq
+       (list (org-time-string-to-seconds "2019-08-19 Mon 09:19") ())))
+)
+
+(r-h (l)
+  (seq-reverse (seq-subseq l 0)))
+
+;; lexical-defun
+(make-logarithmic-histogram (astrotime-list)
+(let* ((numbins
+        (ceiling
+         (log (+ 1.0
+                 (seq-reduce
+                  #'max
+                  (seq-map (lambda (x) (nth 6 x))
+                           (r-h astrotime-list)
+                           )
+                  0))
+              2))))
+  
+  (seq-reduce
+   (lambda (acc elem)
+     (let* ((hardness (nth 6 elem))
+            (nbin (floor (log (+ 1.0 hardness) 2))))
+       (aset acc
+             nbin
+             (+ 1 (aref acc nbin)))
+       acc))
+   (r-h astrotime-list)
+   (make-vector numbins 0)))
+)
+
+;; lexical-defun
+(make-linear-histogram (astrotime-list)
+(let* ((numbins 32)
+       (binsize
+        (ceiling
+         (/ (seq-reduce
+             #'max
+             (seq-map
+              (lambda (x) (nth 6 x))
+              (r-h astrotime-list))
+             0)
+            numbins ))))
+
+  (seq-reduce
+   (lambda (acc elem)
+     (let* ((hardness (nth 6 elem))
+            (nbin (floor (/ hardness binsize))))
+       (aset acc
+             nbin
+             (+ 1 (aref acc nbin)))
+       acc))
+   (r-h astrotime-list)
+   (make-vector numbins 0)))
+)
+
+;; lexical-defun
+(sort-by-hardness (astrotime-list)
+;; 6 is the hardness index
+(seq-sort (lambda (x y)
+            (let* ((hardness-x (nth 6 x))
+                   (hardness-y (nth 6 y)))
+              (if (< hardness-x hardness-y)
+                  t
+                nil)))
+          astrotime-list)
+)
+
+;; lexical-defun
+(sort-by-nsessions (astrotime-list)
+;; 4 is the nsessions index
+(seq-sort (lambda (x y)
+            (let* ((nses-x (nth 4 x))
+                   (nses-y (nth 4 y)))
+              (if (< nses-x nses-y)
+                  t
+                nil)))
+          astrotime-list)
+)
+
+;; lexical-defun
+(sort-by-original-index (astrotime-list)
+;; 8 is the original index
+(seq-sort (lambda (x y)
+            (let* ((oidx-x (nth 8 x))
+                   (oidx-y (nth 8 y)))
+              (if (< oidx-x oidx-y)
+                  t
+                nil)))
+          astrotime-list)
+)
+
 ) ;; end cl-labels defuns
 
 (let* (
@@ -167,142 +326,95 @@
 ;; lexical-define
 (out-of-order-tasks (find-out-of-order-tasks task-seq))
 
+;; lexical-define
+(astrotime-list
+ (summarize-list sorted-task-seq study-sessions))
+
+;; lexical-define
+(logarithmic-histogram
+ (make-logarithmic-histogram astrotime-list))
+
+;; lexical-define
+(linear-histogram
+ (make-linear-histogram astrotime-list))
+
+;; lexical-define
+
+(problems-sorted-by-hardness
+ (sort-by-hardness astrotime-list))
+
+;; lexical-define
+(problems-sorted-by-nsessions
+ (sort-by-nsessions astrotime-list))
+
+;; lexical-define
+(problems-sorted-by-original-index
+ (sort-by-original-index astrotime-list))
+
 )
 
 (insert
-   (let (
-         (astrotime-list
-          (cadr (seq-reduce
-                 (lambda (acc next-elem)
-                   (let ((perv-time-stamp (car acc))
-                         (retval (cadr acc))
-                         (next-time-stamp
-                          (org-time-string-to-seconds
-                           (org-element-property :raw-value (cadr next-elem)))))
-                     ;; loop body
-                     (let ((spans-sessions (seq-reduce
-                                            (lambda (acc next-session)
-                                              (let ((session-start (car next-session))
-                                                    (session-end (cadr next-session)))
-                                                (cond ((<= session-end perv-time-stamp) acc)
-                                                      ((<= next-time-stamp session-start ) acc)
-                                                      (t (list (+ (car acc) 1)
-                                                               (+ (cadr acc)
-                                                                  (cond ((and (<= perv-time-stamp session-start)
-                                                                              (<= session-end next-time-stamp))
-                                                                         (- session-end session-start))
-                                                                        ((and (<= session-start perv-time-stamp)
-                                                                              (<= perv-time-stamp session-end)
-                                                                              (<= session-end next-time-stamp))
-                                                                         (- session-end perv-time-stamp))
-                                                                        ((and (<= perv-time-stamp session-start)
-                                                                              (<= session-start next-time-stamp)
-                                                                              (<= next-time-stamp session-end))
-                                                                         (- next-time-stamp session-start))
-                                                                        ((and (<= session-start perv-time-stamp)
-                                                                              (<= next-time-stamp session-end))
-                                                                         (- next-time-stamp perv-time-stamp))
-                                                                        (t 0))))))
-                                                
-                                                ;; (if (and
-                                                ;;      (<= session-start next-time-stamp)
-                                                ;;      (<= next-time-stamp session-end))
-                                                ;;     t
-                                                ;;   acc)
-                                                ))
-                                            study-sessions
-                                            (list 0 0))))
-                     (list next-time-stamp
-                           (cons (list (car next-elem)  ;; next-elem
-                                       :spent-time-calendar-days (/ (-
-                                                             next-time-stamp
-                                                             perv-time-stamp)
-                                                            (* 60 60 24))
-                                       :spans-sessions (if (not (eq 0 (car spans-sessions)))
-                                                           (car spans-sessions)
-                                                           (error "Fix time: %s, spans-sessions=%s" next-elem spans-sessions))
-                                       :spent-time-net-minutes (/ (cadr spans-sessions) 60))
-                                 retval)))))
-                 sorted-task-seq
-                 (list (org-time-string-to-seconds "2019-08-19 Mon 09:19") ()))))
-         )
+   (let ()
+     (seq-concatenate
+      'string
+      (char-to-string ?\()
+      (pp "Amount of the out-of-order-problems: ")
+      (pp (number-to-string (car out-of-order-tasks)))
+      (char-to-string ?\n)
+      ;;(char-to-string ?\n)
+      (pp "Out-of-order problems :")
+      (char-to-string ?\n)
+      (pp (caddr out-of-order-tasks))
+      ;; (pp (seq-reverse (seq-map (lambda (x) (list (car x))
+      ;;                              )
+      ;;                            (caddr out-of-order-tasks))))
+      (char-to-string ?\n)
 
-     (seq-concatenate 'string
-                      (char-to-string ?\()
-                      (pp "Amount of the out-of-order-problems: ")
-                      (pp (number-to-string (car out-of-order-tasks)))
-                      (char-to-string ?\n)
-                      ;(char-to-string ?\n)
-                      (pp "Out-of-order problems :")
-                      (char-to-string ?\n)
-                      ;;(pp (caddr out-of-order-tasks))
-                      ;; (pp (seq-reverse (seq-map (lambda (x) (list (car x))
-                      ;;                              )
-                      ;;                            (caddr out-of-order-tasks))))
-                      (char-to-string ?\n)
-                      (pp "Task summary:")
-                      (char-to-string ?\n)
-                      ;(pp (seq-reverse (seq-subseq astrotime-list 0 2)))
-                      ;;(decorate-orgtable (seq-reverse (seq-subseq astrotime-list 0)))
-                      ;;(decorate-orgtable (seq-reverse (seq-subseq astrotime-list 0)))
-                      ;; Make a histogram
-                      (pp (let* ((numbins (ceiling (log (+ 1.0 (seq-reduce #'max
-                                                    (seq-map (lambda (x) (nth 6 x))
-                                                             (seq-reverse (seq-subseq astrotime-list 0)))
-                                                    0))
-                                               2))))
-                            
-                        (seq-reduce (lambda (acc elem)
-                                      (let* ((hardness (nth 6 elem))
-                                            (nbin (floor (log (+ 1.0 hardness) 2))))
-                                        (aset acc
-                                              nbin
-                                              (+ 1 (aref acc nbin)))
-                                        acc))
-                         (seq-reverse (seq-subseq astrotime-list 0))
-                         (make-vector numbins 0))
-                        ))
-                      (char-to-string ?\n)
-                      (pp "Median hardness:")
-                      (char-to-string ?\n)
-                      
-                      (pp (let* (;; (numbins (ceiling (log (+ 1.0 (seq-reduce #'max
-                                 ;;                 (seq-map (lambda (x) (nth 6 x))
-                                 ;;                          (seq-reverse (seq-subseq astrotime-list 0)))
-                                 ;;                 0))
-                                 ;;            2)))
-                                 (by-hardness (seq-sort (lambda (x y)
-                                                          (let* ((hardness-x (nth 6 x))
-                                                                 (hardness-y (nth 6 y)))
-                                                            (if (< hardness-x hardness-y)
-                                                                t
-                                                              nil)))
-                                                        astrotime-list)))
-                            
-                        (nth (floor (/ (seq-length by-hardness) 2)) by-hardness)
-                        ))
+      (pp "Task summary (astrotime):")
+      (char-to-string ?\n)
+      (pp (seq-reverse (seq-subseq astrotime-list 0 2)))
+      ;;(decorate-orgtable (seq-reverse (seq-subseq astrotime-list 0)))
+      ;;(decorate-orgtable (seq-reverse (seq-subseq astrotime-list 0)))
 
-                      (pp "Median n-sessions:")
-                      (char-to-string ?\n)
-                      
-                      (pp (let* (;; (numbins (ceiling (log (+ 1.0 (seq-reduce #'max
-                                 ;;                 (seq-map (lambda (x) (nth 6 x))
-                                 ;;                          (seq-reverse (seq-subseq astrotime-list 0)))
-                                 ;;                 0))
-                                 ;;            2)))
-                                 (by-hardness (seq-sort (lambda (x y)
-                                                          (let* ((hardness-x (nth 4 x))
-                                                                 (hardness-y (nth 4 y)))
-                                                            (if (< hardness-x hardness-y)
-                                                                t
-                                                              nil)))
-                                                        astrotime-list)))
-                            
-                        (nth (floor (/ (seq-length by-hardness) 2)) by-hardness)
-                        ))
-                      (char-to-string ?\))
-     ;;(seq-reverse (caddr out-of-order-tasks))
-     )))))
+      (char-to-string ?\n)
+      (pp "Task summary (original-index):")
+      (char-to-string ?\n)
+      (pp (seq-subseq
+           problems-sorted-by-original-index 0 2))
+
+      
+      (pp "Logarithmic histogram:")
+      ;; Make a logarithmic histogram
+      (pp logarithmic-histogram)
+      (char-to-string ?\n)
+      
+      (pp "Linear histogram:")
+      ;; Make a linear histogram
+      (pp linear-histogram)
+      (char-to-string ?\n)
+      
+      (pp "Median hardness:")
+      (char-to-string ?\n)
+      
+      (pp
+       (nth
+        (floor (/ (seq-length
+                   problems-sorted-by-hardness)
+                  2))
+               problems-sorted-by-hardness))
+      
+      (pp "Median n-sessions:")
+      (char-to-string ?\n)
+      
+      (pp
+       (nth
+        (floor (/ (seq-length
+                   problems-sorted-by-nsessions)
+                  2))
+               problems-sorted-by-nsessions))
+      (char-to-string ?\))
+      ;;(seq-reverse (caddr out-of-order-tasks))
+      )))))
 
 
 
