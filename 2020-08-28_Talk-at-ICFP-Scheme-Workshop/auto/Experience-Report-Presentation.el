379466a4d074c718416cfbc2d644520ca68c3274
Lockywolf <lockywolf@gmail.com>
2020-08-08 19:09:57 +0800

    Presentation for ICFP-2020.

---
+++ 2020-08-28_Talk-at-ICFP-Scheme-Workshop/auto/Experience-Report-Presentation.el
@@ -0,0 +1,43 @@
+(TeX-add-style-hook
+ "Experience-Report-Presentation"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation" "CJK" "compress")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "color"
+    "listings")
+   (LaTeX-add-labels
+    "sec:org5377d15"
+    "sec:org4c70654"
+    "sec:orgd0c028c"
+    "sec:orgc508110"))
+ :latex)
+
