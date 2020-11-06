91b5f9783108cd392df80e16b37d837f969561cc
Lockywolf <lockywolf@gmail.com>
2020-11-06 10:05:28 +0800

    Publish last PDF

--- 2020-08-28_Talk-at-ICFP-Scheme-Workshop/auto/Experience-Report-Presentation.el
+++ 2020-08-28_Talk-at-ICFP-Scheme-Workshop/auto/Experience-Report-Presentation.el
@@ -2,21 +2,21 @@
  "Experience-Report-Presentation"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("beamer" "presentation" "CJK" "compress")))
+                     '(("beamer" "presentation" "CJK" "compress" "aspectratio=169")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -33,11 +33,13 @@
     "capt-of"
     "hyperref"
     "color"
-    "listings")
+    "listings"
+    "pgfpages"
+    "supertabular")
    (LaTeX-add-labels
-    "sec:org5377d15"
-    "sec:org4c70654"
-    "sec:orgd0c028c"
-    "sec:orgc508110"))
+    "sec:org62ccca4"
+    "sec:org9ebd68f"
+    "sec:orgfd4b735"
+    "sec:orgff97947"))
  :latex)
 
